510a35fc2b43942d973d9a13556b873848c9756d
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2026-04-22 13:24:43 +0800

    feat: Enhance password input fields with toggle visibility and improved styling for registration form

--- include/client/register.inc.php
+++ include/client/register.inc.php
@@ -97,7 +97,20 @@ $info = Format::htmlchars(($errors && $_POST)?$_POST:$info);
         <span data-i18n="labelCreatePassword"><?php echo __('Create a Password'); ?>:</span>
     </td>
     <td>
-        <input type="password" size="18" name="passwd1" maxlength="128" value="<?php echo $info['passwd1']; ?>">
+        <div class="password-input-wrapper">
+            <input type="password" size="18" name="passwd1" id="register-passwd1" maxlength="128" value="<?php echo $info['passwd1']; ?>" autocomplete="new-password">
+            <button type="button" class="password-toggle" aria-label="<?php echo Format::htmlchars(__('Show password')); ?>"
+                data-label-show="<?php echo Format::htmlchars(__('Show password')); ?>"
+                data-label-hide="<?php echo Format::htmlchars(__('Hide password')); ?>">
+                <svg class="lpb-pw-show-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M10 4C6 4 3.73 6.11 2 9.5C3.73 12.89 6 15 10 15C14 15 16.27 12.89 18 9.5C16.27 6.11 14 4 10 4Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <circle cx="10" cy="9.5" r="2.5" stroke="currentColor" stroke-width="1.5"/>
+                </svg>
+                <svg class="lpb-pw-hide-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" style="display: none;" aria-hidden="true">
+                    <path d="M2 2L18 18M8.88 8.88C8.3 9.46 8 10.22 8 11C8 12.66 9.34 14 11 14C11.78 14 12.54 13.7 13.12 13.12M14.71 14.71C13.5 15.53 11.85 16 10 16C6 16 3.73 13.89 2 10.5C2.64 9.19 3.5 8.06 4.5 7.19M7.53 7.53C7.19 7.87 7 8.41 7 9C7 10.66 8.34 12 10 12C10.59 12 11.13 11.81 11.47 11.47" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
         &nbsp;<span class="error">&nbsp;<?php echo $errors['passwd1']; ?></span>
     </td>
 </tr>
@@ -106,7 +119,20 @@ $info = Format::htmlchars(($errors && $_POST)?$_POST:$info);
         <span data-i18n="labelConfirmPassword"><?php echo __('Confirm New Password'); ?>:</span>
     </td>
     <td>
-        <input type="password" size="18" name="passwd2" maxlength="128" value="<?php echo $info['passwd2']; ?>">
+        <div class="password-input-wrapper">
+            <input type="password" size="18" name="passwd2" id="register-passwd2" maxlength="128" value="<?php echo $info['passwd2']; ?>" autocomplete="new-password">
+            <button type="button" class="password-toggle" aria-label="<?php echo Format::htmlchars(__('Show password')); ?>"
+                data-label-show="<?php echo Format::htmlchars(__('Show password')); ?>"
+                data-label-hide="<?php echo Format::htmlchars(__('Hide password')); ?>">
+                <svg class="lpb-pw-show-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M10 4C6 4 3.73 6.11 2 9.5C3.73 12.89 6 15 10 15C14 15 16.27 12.89 18 9.5C16.27 6.11 14 4 10 4Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <circle cx="10" cy="9.5" r="2.5" stroke="currentColor" stroke-width="1.5"/>
+                </svg>
+                <svg class="lpb-pw-hide-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" style="display: none;" aria-hidden="true">
+                    <path d="M2 2L18 18M8.88 8.88C8.3 9.46 8 10.22 8 11C8 12.66 9.34 14 11 14C11.78 14 12.54 13.7 13.12 13.12M14.71 14.71C13.5 15.53 11.85 16 10 16C6 16 3.73 13.89 2 10.5C2.64 9.19 3.5 8.06 4.5 7.19M7.53 7.53C7.19 7.87 7 8.41 7 9C7 10.66 8.34 12 10 12C10.59 12 11.13 11.81 11.47 11.47" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
         &nbsp;<span class="error">&nbsp;<?php echo $errors['passwd2']; ?></span>
     </td>
 </tr>
@@ -131,4 +157,38 @@ $(function() {
     $('#timezone-dropdown').val(zone.name()).trigger('change');
 });
 </script>
-<?php }
+<?php } ?>
+<script>
+(function () {
+    function bindRegisterPasswordToggles() {
+        document.querySelectorAll('.lpb-register-form .password-input-wrapper').forEach(function (wrap) {
+            var input = wrap.querySelector('input');
+            var btn = wrap.querySelector('.password-toggle');
+            if (!input || !btn || btn.getAttribute('data-lpb-pw-bound') === '1') {
+                return;
+            }
+            btn.setAttribute('data-lpb-pw-bound', '1');
+            var eyeShow = btn.querySelector('.lpb-pw-show-icon');
+            var eyeHide = btn.querySelector('.lpb-pw-hide-icon');
+            var labelShow = btn.getAttribute('data-label-show') || '';
+            var labelHide = btn.getAttribute('data-label-hide') || '';
+            btn.addEventListener('click', function () {
+                var show = input.getAttribute('type') === 'password';
+                input.setAttribute('type', show ? 'text' : 'password');
+                if (eyeShow) {
+                    eyeShow.style.display = show ? 'none' : '';
+                }
+                if (eyeHide) {
+                    eyeHide.style.display = show ? '' : 'none';
+                }
+                btn.setAttribute('aria-label', show ? labelHide : labelShow);
+            });
+        });
+    }
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', bindRegisterPasswordToggles);
+    } else {
+        bindRegisterPasswordToggles();
+    }
+})();
+</script>
