8969d9a985c280dcedf7d2c92584898912db3ea1
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2026-04-22 14:58:11 +0800

    feat: Enhance navigation and user account features with new links and improved styling

--- include/client/lpb-chrome-nav-account.inc.php
+++ include/client/lpb-chrome-nav-account.inc.php
@@ -5,10 +5,10 @@ if (!defined('OSTCLIENTINC')) {
 ?>
 <?php if ($client_logged_in) { ?>
                 <span class="nav-link"><?php echo Format::htmlchars($thisclient->getName()); ?></span>
-                <a href="<?php echo Format::htmlchars(ROOT_PATH); ?>profile.php" class="nav-link"><?php echo __('Profile'); ?></a>
-                <a href="<?php echo Format::htmlchars(ROOT_PATH); ?>tickets.php" class="nav-link"><?php
-                    echo sprintf(__('Tickets (%d)'), $thisclient->getNumTickets()); ?></a>
-                <a href="<?php echo Format::htmlchars($signout_url); ?>" class="nav-link login-link"><?php echo __('Sign Out'); ?></a>
+                <a href="<?php echo Format::htmlchars(ROOT_PATH); ?>profile.php" class="nav-link"><span data-i18n="navProfile">Profil</span></a>
+                <a href="<?php echo Format::htmlchars(ROOT_PATH); ?>tickets.php" class="nav-link"><span data-i18n="navTickets">Tiket</span> (<span class="lpb-nav-ticket-count"><?php
+                    echo (int) $thisclient->getNumTickets(); ?></span>)</a>
+                <a href="<?php echo Format::htmlchars($signout_url); ?>" class="nav-link login-link lpb-nav-signout"><span data-i18n="navSignOut">Keluar</span></a>
 <?php } else { ?>
                 <a href="<?php echo Format::htmlchars($signin_url); ?>" class="nav-link login-link" data-i18n="navLogin">Masuk</a>
 <?php } ?>
